dd9dde17b37bf0467f120c9b4ee3b998f2bea658
William Payne <72815043+Wjpayne@users.noreply.github.com>
2025-09-01 21:53:41 +0000

    setup mongoUri

--- server/index.js
+++ server/index.js
@@ -3,7 +3,9 @@ import mongoose from "mongoose";
 import cors from "cors";
 import itemsRouter from "./routes/items.js";
 
+
 const app = express();
+const mongoUri = process.env.REACT_APP_MONGO_URI;
 
 // Middleware
 app.use(cors());
@@ -14,8 +16,8 @@ app.use("/items", itemsRouter);
 
 // MongoDB connection
 mongoose
-  .connect("mongodb://localhost:27017/coffee_inventory")
+  .connect(mongoUri)
   .then(() => console.log("✅ MongoDB connected"))
   .catch((err) => console.error("❌ MongoDB error:", err));
 
-app.listen(4000, () => console.log("🚀 Server running on http://localhost:4000"));
+app.listen(process.env.PORT, () => console.log("🚀 Server running on http://localhost:4000"));
